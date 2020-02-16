00d8e98a3e11ff45ac28befbed118299cabb9bc5
EnricoAffuso <EnricoAffuso>
2020-02-16 21:58:39 +0100

    su per favore

---
+++ Scripts/CameraBheaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBheaviour : MonoBehaviour
+{
+
+    Transform Player;
+
+    [SerializeField]
+    float z_offset = -100;
+    [SerializeField]
+    float y_offset = 2;
+
+    [SerializeField]
+    float smooth = 0.1f;
+
+    Vector3 movimento = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player != null)
+        {
+            Vector3 posizione = new Vector3(Player.position.x, Player.position.y + y_offset, Player.position.z + z_offset);
+            transform.position = Vector3.SmoothDamp(transform.position, posizione, ref movimento, smooth);
+        }
+    }
+}
---
+++ Scripts/Movimenti.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class Movimenti : MonoBehaviour
+{
+
+    Rigidbody2D body;
+    Animator anim;
+
+    //Variabili Migliorabili
+    [SerializeField]
+    float speed = 10;
+    [SerializeField]
+    float jump = 40f;
+
+    bool staSaltando = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimenti();
+        salto();
+    }
+
+    void movimenti()
+    {
+        float movimentoOrizzontale = Input.GetAxis("Horizontal");
+        Vector2 direzione = new Vector2(0, 0);
+
+        if(movimentoOrizzontale != 0)
+        {
+            direzione.x = movimentoOrizzontale; 
+        }
+        transform.Translate(direzione * (speed * Time.deltaTime));
+    
+        if(direzione.x >= 0)
+        {
+            body.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            body.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        anim.SetFloat("siMuove", Mathf.Abs(movimentoOrizzontale));
+    }
+
+    void salto()
+    {
+        float saltoVerticale = Input.GetAxis("Jump");
+        if (staSaltando)
+        {
+            anim.SetFloat("siMuove", Mathf.Abs(0));
+            if (body.velocity.y == 0)
+            {
+                anim.SetBool("jumping", false);
+                staSaltando = false;
+            }
+        }
+        else
+        {
+            if(saltoVerticale > 0)
+            {
+                anim.SetBool("jumping", true);
+                body.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+                staSaltando = true;
+            }
+        }
+    }
+}
